4ccc8e61660b90fbae522593a81fe1d485a30675
darzwaitz <darzwaitz@yahoo.com>
2023-07-05 22:33:50 +0100

    using one file for all components

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,11 +1,34 @@
-import "./App.css";
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>starter</h1>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>Far away</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your next trip</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>You have X items on your list...</em>
+    </footer>
+  );
+}
