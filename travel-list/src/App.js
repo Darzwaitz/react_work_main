f7bf835b46e172a6f26644fe895ac00e53e46be2
darzwaitz <darzwaitz@yahoo.com>
2023-07-11 23:54:11 +0100

    conditionally displaying how many items packed - and percentage

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -127,13 +127,19 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 function Stats({ items }) {
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
+  const percentPacked = Math.round(numPacked / numItems) * 100;
 
   return (
     <footer className="stats">
       <em>
-        You have {numItems} item{numItems === 1 ? "" : "s"} on your list...and
-        you have packed&nbsp;
-        {numPacked} item{numPacked === 1 ? "" : "s"}
+        {percentPacked === 100
+          ? "Everything packed!"
+          : ` You have ${numItems} item${
+              numItems === 1 ? "" : "s"
+            } on your list...and
+        you have packed 
+        ${numPacked} item${numPacked === 1 ? "" : "s"} - (
+        ${percentPacked ? percentPacked : 0}%)`}
       </em>
     </footer>
   );
