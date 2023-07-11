6de1384bd806845563c63383b98a59084c136778
darzwaitz <darzwaitz@yahoo.com>
2023-07-11 23:57:17 +0100

    conditionally render footer element

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -125,6 +125,13 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 }
 
 function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding items...</em>
+      </p>
+    );
+
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
   const percentPacked = Math.round(numPacked / numItems) * 100;
