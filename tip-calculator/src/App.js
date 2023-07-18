6d33c8181a988dfeb516952b8b9824af495c83e6
darzwaitz <darzwaitz@yahoo.com>
2023-07-18 23:21:01 +0100

    creating main comps

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -3,11 +3,49 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>start</h1>
-      </header>
+      <TipCalculator />
     </div>
   );
+}
+
+function TipCalculator() {
+  return (
+    <div>
+      <BillInput />
+      <SelectPercentage>How did you like the service</SelectPercentage>
+      <SelectPercentage>How dif your friend like the service</SelectPercentage>
+      <Output />
+      <Reset />
+    </div>
+  );
+}
+function BillInput() {
+  return (
+    <div>
+      <label>How much was the bill</label>
+      <input type="text" placeholder="Bill Value" />
+    </div>
+  );
+}
+
+function SelectPercentage({ children }) {
+  return (
+    <div>
+      <label>{children}</label>
+      <select>
+        <option value="0">Dissatified (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+function Output() {
+  return <h1>shtart</h1>;
+}
+function Reset() {
+  return <h1>shtart</h1>;
 }
 
 export default App;
