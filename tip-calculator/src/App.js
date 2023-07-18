ee15b6e4e2ebee2816d4c74ae1d04375625b97d9
darzwaitz <darzwaitz@yahoo.com>
2023-07-18 23:31:01 +0100

    creating state - Part 1

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -9,9 +10,11 @@ function App() {
 }
 
 function TipCalculator() {
+  const [bill, setBill] = useState("");
+
   return (
     <div>
-      <BillInput />
+      <BillInput bill={bill} onSetBill={setBill} />
       <SelectPercentage>How did you like the service</SelectPercentage>
       <SelectPercentage>How dif your friend like the service</SelectPercentage>
       <Output />
@@ -19,11 +22,16 @@ function TipCalculator() {
     </div>
   );
 }
-function BillInput() {
+function BillInput({ bill, onSetBill }) {
   return (
     <div>
       <label>How much was the bill</label>
-      <input type="text" placeholder="Bill Value" />
+      <input
+        type="text"
+        placeholder="Bill Value"
+        value={bill}
+        onChange={(e) => onSetBill(Number(e.target.value))}
+      />
     </div>
   );
 }
