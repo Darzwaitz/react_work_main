5d249d018c3b3cd99fa5666753348b03ae39dd4d
darzwaitz <darzwaitz@yahoo.com>
2023-07-19 22:41:37 +0100

    updating state etc - TBU

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -11,13 +11,19 @@ function App() {
 
 function TipCalculator() {
   const [bill, setBill] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
 
   return (
     <div>
       <BillInput bill={bill} onSetBill={setBill} />
-      <SelectPercentage>How did you like the service</SelectPercentage>
-      <SelectPercentage>How dif your friend like the service</SelectPercentage>
-      <Output />
+      <SelectPercentage percentage={percentage1} onSelect={setPercentage1}>
+        How did you like the service
+      </SelectPercentage>
+      <SelectPercentage percentage={percentage2} onSelect={setPercentage2}>
+        How dif your friend like the service
+      </SelectPercentage>
+      <Output bill={bill} />
       <Reset />
     </div>
   );
@@ -36,11 +42,14 @@ function BillInput({ bill, onSetBill }) {
   );
 }
 
-function SelectPercentage({ children }) {
+function SelectPercentage({ children, percentage, onSelect }) {
   return (
     <div>
       <label>{children}</label>
-      <select>
+      <select
+        value={percentage}
+        onChange={(e) => onSelect(Number(e.target.value))}
+      >
         <option value="0">Dissatified (0%)</option>
         <option value="5">It was okay (5%)</option>
         <option value="10">It was good (10%)</option>
@@ -49,8 +58,8 @@ function SelectPercentage({ children }) {
     </div>
   );
 }
-function Output() {
-  return <h3>You pay X ($A + $B tip)</h3>;
+function Output({ bill }) {
+  return <h3>You pay X (${bill} + $B tip)</h3>;
 }
 function Reset() {
   return <button>Reset</button>;
