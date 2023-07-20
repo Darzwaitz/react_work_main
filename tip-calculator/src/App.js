ffb2e00f19224b352195a2893b6c4909e2ca6f09
darzwaitz <darzwaitz@yahoo.com>
2023-07-20 22:25:28 +0100

    finishing logic & adding reset functionality

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -14,6 +14,14 @@ function TipCalculator() {
   const [percentage1, setPercentage1] = useState(0);
   const [percentage2, setPercentage2] = useState(0);
 
+  const tip = bill * ((percentage1 + percentage2) / 2 / 100);
+
+  function handleReset() {
+    setBill("");
+    setPercentage1(0);
+    setPercentage2(0);
+  }
+
   return (
     <div>
       <BillInput bill={bill} onSetBill={setBill} />
@@ -21,10 +29,10 @@ function TipCalculator() {
         How did you like the service
       </SelectPercentage>
       <SelectPercentage percentage={percentage2} onSelect={setPercentage2}>
-        How dif your friend like the service
+        How did your friend like the service
       </SelectPercentage>
-      <Output bill={bill} />
-      <Reset />
+      <Output bill={bill} tip={tip} />
+      <Reset onReset={handleReset} />
     </div>
   );
 }
@@ -58,11 +66,15 @@ function SelectPercentage({ children, percentage, onSelect }) {
     </div>
   );
 }
-function Output({ bill }) {
-  return <h3>You pay X (${bill} + $B tip)</h3>;
+function Output({ bill, tip }) {
+  return (
+    <h3>
+      You pay {bill + tip} (${bill} + {tip})
+    </h3>
+  );
 }
-function Reset() {
-  return <button>Reset</button>;
+function Reset({ onReset }) {
+  return <button onClick={onReset}>Reset</button>;
 }
 
 export default App;
