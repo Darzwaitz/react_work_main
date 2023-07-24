a23f40e622e1b0d9f6f787791edc17bb10eef926
darzwaitz <darzwaitz@yahoo.com>
2023-07-24 22:24:58 +0100

    setting up the form comp - Part 2 - UPDATED

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -24,8 +24,12 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendList />
+
         <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+
+      <FormSplitBill />
     </div>
   );
 }
@@ -72,13 +76,13 @@ function Button({ children }) {
 function FormAddFriend() {
   return (
     <form className="form-add-friend">
-      <label>Friend name</label>
+      <label>🍟 Friend Name</label>
       <input type="text" />
 
-      <label>Img Url</label>
+      <label>🖼 Img Url</label>
       <input type="text" />
 
-      <button className="button">Add</button>
+      <Button>Add</Button>
     </form>
   );
 }
@@ -86,7 +90,23 @@ function FormAddFriend() {
 function FormSplitBill() {
   return (
     <form className="form-split-bill">
-      <h2>Split a bill with</h2>
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill Value</label>
+      <input type="text" />
+
+      <label>🧑🏻 Your Expenses</label>
+      <input type="text" />
+
+      <label>🤼 X's Expenses</label>
+      <input type="text" disabled />
+
+      <label>💰 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+      <Button>Add</Button>
     </form>
   );
 }
