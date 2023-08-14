1b1a3d945ce78e97091b853f080a7f9cc955a732
darzwaitz <darzwaitz@yahoo.com>
2023-08-14 20:54:40 +0100

    refactoring to make the comp reusable - Part 1

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -9,8 +9,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    {/* <StarRating maxRating={10} />
-    <StarRating /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" />
+    {/* <StarRating /> */}
   </React.StrictMode>
 );
