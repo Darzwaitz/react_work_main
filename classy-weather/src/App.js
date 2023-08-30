8ae6306910a67ed7d0a09d6dba0527ede91917ee
darzwaitz <darzwaitz@yahoo.com>
2023-08-30 21:25:56 +0100

    adding geolocation functionality - Part 3 - creating weather comp from data returned - TBU

--- classy-weather/src/App.js
+++ classy-weather/src/App.js
@@ -91,9 +91,26 @@ class App extends React.Component {
         </div>
         <button onClick={this.fetchWeather}>Get Weather</button>
         {this.state.isLoading && <p className="loader">Loading...</p>}
+        {this.state.weather.weatherCode && (
+          <Weather
+            weather={this.state.weather}
+            location={this.state.location}
+          />
+        )}
+        )
       </div>
     );
   }
 }
 
 export default App;
+
+class Weather extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Weather</h2>
+      </div>
+    );
+  }
+}
