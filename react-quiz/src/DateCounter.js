9db3e01eb95ad14a6b77845be1d872cd25545d90
darzwaitz <darzwaitz@yahoo.com>
2023-09-05 22:21:26 +0100

    using useReducer for state - Part 2 - TBU

--- react-quiz/src/DateCounter.js
+++ react-quiz/src/DateCounter.js
@@ -2,7 +2,10 @@ import { useReducer, useState } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  return state + action;
+  // return state + action;
+  if (action.type === "inc") return state + action.payload;
+  if (action.type === "dec") return state - action.payload;
+  if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
@@ -16,18 +19,21 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
+    dispatch({ type: "dec", payload: -1 });
+
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
 
   const inc = function () {
-    dispatch(1);
+    dispatch({ type: "inc", payload: 1 });
 
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
     // setCount(Number(e.target.value));
   };
 
