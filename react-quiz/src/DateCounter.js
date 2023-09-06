e56152499789cb9f96c354e3041dff2406fe9848
darzwaitz <darzwaitz@yahoo.com>
2023-09-06 16:24:24 +0100

    using a switch statement in useReducer reducer function - Part 2

--- react-quiz/src/DateCounter.js
+++ react-quiz/src/DateCounter.js
@@ -1,16 +1,22 @@
 // import { useReducer, useState } from "react";
 import { useReducer } from "react";
 
+const initialState = { count: 0, step: 1 };
+
 function reducer(state, action) {
   console.log(state, action);
 
   switch (action.type) {
     case "dec":
-      return { ...state, count: state.count - 1 };
+      return { ...state, count: state.count - state.step };
     case "inc":
-      return { ...state, count: state.count + 1 };
+      return { ...state, count: state.count + state.step };
     case "setCount":
       return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
     default:
       throw new Error("Unknown action");
   }
@@ -21,7 +27,6 @@ function DateCounter() {
   // const [count, dispatch] = useReducer(reducer, 0);
   // const [step, setStep] = useState(1);
 
-  const initialState = { count: 0, step: 1 };
   const [state, dispatch] = useReducer(reducer, initialState);
   const { count, step } = state;
 
@@ -49,10 +54,14 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
+
     // setStep(Number(e.target.value));
   };
 
   const reset = function () {
+    dispatch({ type: "reset" });
+
     // setCount(0);
     // setStep(1);
   };
