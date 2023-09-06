3f10b31cce3df8a762b7f3e7539ac54c453880f0
darzwaitz <darzwaitz@yahoo.com>
2023-09-06 16:14:42 +0100

    using a switch statement in useReducer reducer function - Part 1

--- react-quiz/src/DateCounter.js
+++ react-quiz/src/DateCounter.js
@@ -1,18 +1,29 @@
-import { useReducer, useState } from "react";
+// import { useReducer, useState } from "react";
+import { useReducer } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  // return state + action;
-  if (action.type === "inc") return state + action.payload;
-  if (action.type === "dec") return state - action.payload;
-  if (action.type === "setCount") return action.payload;
+
+  switch (action.type) {
+    case "dec":
+      return { ...state, count: state.count - 1 };
+    case "inc":
+      return { ...state, count: state.count + 1 };
+    case "setCount":
+      return { ...state, count: action.payload };
+    default:
+      throw new Error("Unknown action");
+  }
 }
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer(reducer, 0);
+  // const [count, dispatch] = useReducer(reducer, 0);
+  // const [step, setStep] = useState(1);
 
-  const [step, setStep] = useState(1);
+  const initialState = { count: 0, step: 1 };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -38,12 +49,12 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
   };
 
   return (
