640d80fc2c3a066f8e44850700c2bbba5a824396
darzwaitz <darzwaitz@yahoo.com>
2023-09-07 22:08:03 +0100

    installed json server package & set up fetch to grab json data

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,8 +1,16 @@
 // import DateCounter from "./DateCounter";
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
 function App() {
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error"));
+  });
+
   return (
     <div className="app">
       <Header />
