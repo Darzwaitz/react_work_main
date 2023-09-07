41dc8f5dbb352ae1a2232be694a4eab9e275ec91
darzwaitz <darzwaitz@yahoo.com>
2023-09-07 22:19:58 +0100

    using useReducer on json data - TBU

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,13 +1,33 @@
 // import DateCounter from "./DateCounter";
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        question: action.payload,
+        status: "ready",
+      };
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(function () {
     fetch("http://localhost:9000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => console.error("Error"));
   });
 
