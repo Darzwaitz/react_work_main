f6f831e63d6507fff91d41111c1463dadab11273
darzwaitz <darzwaitz@yahoo.com>
2023-09-11 22:15:46 +0100

    Loading questions - Part 3 - TBU

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -48,7 +49,7 @@ function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <Error />}
+        {status === "ready" && <StartScreen />}
       </Main>
     </div>
   );
