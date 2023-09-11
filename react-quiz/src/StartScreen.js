8a117d7ee759283a802125359a56ed7a39cf2b7b
darzwaitz <darzwaitz@yahoo.com>
2023-09-11 22:08:48 +0100

    Loading questions - Part 2 - create StartScreen comp

---
+++ react-quiz/src/StartScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function StartScreen() {
+  return (
+    <div>
+      <h2>Welcome to the React quiz</h2>
+      <h3>X questions to test your React mastery</h3>
+      <button>Let's start</button>
+    </div>
+  );
+}
+
+export default StartScreen;
