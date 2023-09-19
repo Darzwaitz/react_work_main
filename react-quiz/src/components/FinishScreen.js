1d5ecdb9193eb5555db280a1981556714a16f5c2
darzwaitz <darzwaitz@yahoo.com>
2023-09-19 23:25:44 +0100

    percentage score & emoji on FinishScreen comp

--- react-quiz/src/components/FinishScreen.js
+++ react-quiz/src/components/FinishScreen.js
@@ -1,9 +1,16 @@
 function FinishScreen({ points, maxPossiblePoints }) {
   const percentage = (points / maxPossiblePoints) * 100;
 
+  let emoji;
+  if (percentage === 100) emoji = "😃";
+  if (percentage >= 80 && percentage < 100) emoji = "😊";
+  if (percentage >= 50 && percentage < 80) emoji = "🙂";
+  if (percentage >= 0 && percentage < 50) emoji = "🤔";
+  if (percentage === 0) emoji = "😭";
+
   return (
-    <p>
-      You scored <strong>{points}</strong> out of {maxPossiblePoints} (
+    <p className="result">
+      {emoji} You scored <strong>{points}</strong> out of {maxPossiblePoints} (
       {Math.ceil(percentage)}%)
     </p>
   );
