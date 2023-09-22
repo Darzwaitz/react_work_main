6936465e0f63a110dd568a4f7ad2d3a1a6ef3324
darzwaitz <darzwaitz@yahoo.com>
2023-09-22 22:00:49 +0100

    formatting time for proper UI output

--- react-quiz/src/components/Timer.js
+++ react-quiz/src/components/Timer.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 
 function Timer({ dispatch, secondsRemaining }) {
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+
   useEffect(() => {
     const id = setInterval(() => {
       // console.log("tick");
@@ -10,7 +13,11 @@ function Timer({ dispatch, secondsRemaining }) {
     return () => clearInterval(id);
   }, [dispatch]);
 
-  return <div className="timer">{secondsRemaining}</div>;
+  return (
+    <div className="timer">
+      {mins < 10 && "0"} {mins} : {seconds < 10 && "0"} {seconds}
+    </div>
+  );
 }
 
 export default Timer;
