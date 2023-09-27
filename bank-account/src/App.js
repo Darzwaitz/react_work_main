5f037f0819174aee5821e06781c84ec09234efa8
darzwaitz <darzwaitz@yahoo.com>
2023-09-27 23:48:19 +0100

    setting up events - Part 5 - close account

--- bank-account/src/App.js
+++ bank-account/src/App.js
@@ -50,7 +50,8 @@ function reducer(state, action) {
     case "payLoan":
       return { ...state, loan: 0, balance: state.balance - state.loan };
     case "closeAccount":
-      return "";
+      if (state.loan > 0 && state.balance !== 0) return state;
+      return initialState;
     default:
       throw new Error("Unknown");
   }
