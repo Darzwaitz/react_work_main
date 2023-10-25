ddf06f443f172cbf359b27b565ce52e804cceba5
darzwaitz <darzwaitz@yahoo.com>
2023-10-25 23:56:33 +0100

    refactoring & creating a Context - Part 3

--- world-wise/src/contexts/CitiesContext.jsx
+++ world-wise/src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -30,5 +30,11 @@ function CitiesProvider({ children }) {
     </CitiesContext.Provider>
   );
 }
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside the CitiesProvider");
+  return context;
+}
 
-export { CitiesProvider };
+export { CitiesProvider, useCities };
