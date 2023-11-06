ee9ef496472e61c12a51f7adb66867e0669c5870
darzwaitz <darzwaitz@yahoo.com>
2023-11-06 23:35:32 +0000

    using leaflet comp - fixed with added css class - need to fix error onClick of map

--- world-wise/src/components/Map.jsx
+++ world-wise/src/components/Map.jsx
@@ -14,10 +14,15 @@ function Map() {
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={false}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.mapContainer}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
         <Marker position={mapPosition}>
           <Popup>
