565456c6f95227afdfc9f27d63cdd568c66d3735
darzwaitz <darzwaitz@yahoo.com>
2023-11-09 23:57:44 +0000

    creating a DetectClick function - Part 2

--- world-wise/src/components/Map.jsx
+++ world-wise/src/components/Map.jsx
@@ -64,7 +64,7 @@ function DetectClick() {
   const navigate = useNavigate();
 
   useMapEvents({
-    click: (e) => navigate(`form`),
+    click: (e) => navigate(`form?lat=${e.latlng.lat}`),
   });
 }
 
