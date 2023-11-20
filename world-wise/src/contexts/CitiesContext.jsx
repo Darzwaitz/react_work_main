3803a0f6c8089d4f926307916d0b569f5d320e07
darzwaitz <darzwaitz@yahoo.com>
2023-11-20 21:54:59 +0000

    creating new fetch request to post to temp fake server - Part 1

--- world-wise/src/contexts/CitiesContext.jsx
+++ world-wise/src/contexts/CitiesContext.jsx
@@ -38,6 +38,22 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+    } catch {
+      alert("error loading data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
       {children}
