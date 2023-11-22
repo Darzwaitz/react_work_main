d7aa65bbe57d9c248bc10cbd16027bd023112b6b
darzwaitz <darzwaitz@yahoo.com>
2023-11-22 23:02:38 +0000

    creating state management with Context & useReducer hooks - Part 1

--- world-wise/src/contexts/CitiesContext.jsx
+++ world-wise/src/contexts/CitiesContext.jsx
@@ -4,6 +4,12 @@ const BASE_URL = "http://localhost:9000";
 
 const CitiesContext = createContext();
 
+const initialState = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+};
+
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
