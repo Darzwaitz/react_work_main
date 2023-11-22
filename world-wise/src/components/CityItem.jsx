cba44177c202bc275bc26016c717de9be94a80e1
darzwaitz <darzwaitz@yahoo.com>
2023-11-22 22:50:21 +0000

    deleting a city - Part 1

--- world-wise/src/components/CityItem.jsx
+++ world-wise/src/components/CityItem.jsx
@@ -12,6 +12,12 @@ const formatDate = (date) =>
 function CityItem({ city }) {
   const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log("testty");
+  }
+
   return (
     <li
       className={`${styles.cityItem} ${
@@ -23,7 +29,9 @@ function CityItem({ city }) {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={handleClick}>
+          &times;
+        </button>
       </Link>
     </li>
   );
