98af875d90571d6e88a9e360ed73bc1a854b1982
darzwaitz <darzwaitz@yahoo.com>
2023-12-08 23:57:07 +0000

    using react useMemo on v1 of the app - Part 2

--- atomic-blog/src/App.js
+++ atomic-blog/src/App.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, memo, useContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { PostProvider, usePosts } from "./PostContext";
 import Test from "./Test";
@@ -74,7 +74,7 @@ function Results() {
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
-function Main() {
+const Main = memo(function Main() {
   // const { posts, onAddPost } = usePosts();
 
   return (
@@ -83,7 +83,7 @@ function Main() {
       <Posts />
     </main>
   );
-}
+});
 
 function Posts() {
   return (
