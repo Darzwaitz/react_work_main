49dc48eef5d1400d776e114db80a2915cb2c6690
darzwaitz <darzwaitz@yahoo.com>
2023-12-11 23:00:41 +0000

    using react useCallback to prevent infinite loop - Part 1

--- world-wise/src/contexts/CitiesContext.jsx
+++ world-wise/src/contexts/CitiesContext.jsx
@@ -1,9 +1,9 @@
 import {
   createContext,
-  useState,
   useEffect,
   useContext,
   useReducer,
+  useCallback,
 } from "react";
 
 const BASE_URL = "http://localhost:9000";
@@ -78,20 +78,23 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (Number(id) === currentCity.id) return;
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
 
-    dispatch({ type: "loading" });
-
-    try {
-      setIsLoading(true);
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
       dispatch({ type: "loading" });
-    } catch {
-      dispatch({ type: "rejected", payload: "error loading data..." });
-    }
-  }
+
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: "loading" });
+      } catch {
+        dispatch({ type: "rejected", payload: "error loading data..." });
+      }
+    },
+    [currentCity.id]
+  );
 
   async function createCity(newCity) {
     dispatch({ type: "loading" });
