7fc4cd6ab356bc1a82bbbe2d3482032b2a401418
darzwaitz <darzwaitz@yahoo.com>
2023-12-19 20:40:00 +0000

    refactoring react useCallback to use useEffect instead

--- workout-timer/src/Calculator.js
+++ workout-timer/src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useCallback, useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import clickSound from "./ClickSound.m4a";
 
 function Calculator({ workouts, allowSound }) {
@@ -9,22 +9,23 @@ function Calculator({ workouts, allowSound }) {
 
   const [duration, setDuration] = useState(0);
 
-  const playSound = useCallback(
+  useEffect(
     function () {
-      if (!allowSound) return;
-      const sound = new Audio(clickSound);
-      sound.play();
+      setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
     },
-    [allowSound]
+    [number, sets, speed, durationBreak]
   );
 
   useEffect(
     function () {
-      setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
-
+      const playSound = function () {
+        if (!allowSound) return;
+        const sound = new Audio(clickSound);
+        sound.play();
+      };
       playSound();
     },
-    [number, sets, speed, durationBreak, playSound]
+    [duration, allowSound]
   );
 
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
@@ -33,12 +34,10 @@ function Calculator({ workouts, allowSound }) {
 
   function handleInc() {
     setDuration((duration) => Math.floor(duration) + 1);
-    playSound();
   }
 
   function handleDec() {
     setDuration((duration) => (duration > 1 ? Math.ceil(duration) - 1 : 0));
-    playSound();
   }
 
   return (
