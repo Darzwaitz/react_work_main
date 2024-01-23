c00e4ec8dc5b62bd49b6730641be655d96c15d13
darzwaitz <darzwaitz@yahoo.com>
2024-01-23 19:01:03 +0000

    refactoring to add another createSlice to reduxjs toolkit

--- redux-intro/src/features/customers/CreateCustomer.js
+++ redux-intro/src/features/customers/CreateCustomer.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+// import { createCustomer } from "./customerSlice";
 import { createCustomer } from "./customerSlice";
 
 function Customer() {
