c049233a8e4a62a977426a17536f26e1d3515315
darzwaitz <darzwaitz@yahoo.com>
2024-01-25 23:50:10 +0000

    installed react-router-dom@6 and set up routes - Part 2 setting up routes

--- pizza-shop/src/App.jsx
+++ pizza-shop/src/App.jsx
@@ -1,6 +1,9 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
 
 const router = createBrowserRouter([
   {
@@ -8,6 +11,9 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   { path: "/menu", element: <Menu /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:orderId", element: <Order /> },
 ]);
 
 function App() {
