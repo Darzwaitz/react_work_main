3581aadf0db7b70c590bdee9a713cc98a00aab08
darzwaitz <darzwaitz@yahoo.com>
2024-01-29 23:41:47 +0000

    using useLoaderData from react-router-dom & a loader func as convention is passed in

--- pizza-shop/src/features/menu/Menu.jsx
+++ pizza-shop/src/features/menu/Menu.jsx
@@ -1,9 +1,18 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+
 function Menu() {
+  const menu = useLoaderData();
+  console.log(menu);
+
   return <h1>Menu</h1>;
 }
 
-export default function loader() {
+// loader function grabs data for useLoaderData
+// named 'loader' as convention
+export async function loader() {
   const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
