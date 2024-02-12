ca83cde5967d39665200309b6226840b8bdd55e3
darzwaitz <darzwaitz@yahoo.com>
2024-02-12 23:24:01 +0000

    setting up tailwind - installing packages and creating config files - TBF

---
+++ pizza-shop/prettier.config.cjs
@@ -0,0 +1,9 @@
+// prettier.config.js
+// module.exports = {
+//   plugins: [require("prettier-plugin-tailwindcss")],
+// };
+// eslint-disable-next-line no-undef
+module.exports = {
+  // eslint-disable-next-line no-undef
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
