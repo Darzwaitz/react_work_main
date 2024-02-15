f9a6b80a9d274cc0127abc9e75870c286edce89e
darzwaitz <darzwaitz@yahoo.com>
2024-02-15 21:19:10 +0000

    updated prettier config file to a JSON file and added some tailwind stylez

--- pizza-shop/src/ui/Header.jsx
+++ pizza-shop/src/ui/Header.jsx
@@ -1,14 +1,18 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom'
+import SearchOrder from '../features/order/SearchOrder'
 
 function Header() {
-  return (
-    <header>
-      <Link to={"/"}>Pizza shop</Link>
+    return (
+        <header className="bg-yellow-500 uppercase">
+            <Link to="/" className="tracking-[5px]">
+                Pizza shop
+            </Link>
 
-      <SearchOrder />
-    </header>
-  );
+            <SearchOrder />
+
+            <p>Darz</p>
+        </header>
+    )
 }
 
-export default Header;
+export default Header
