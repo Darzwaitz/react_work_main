a9f05a7ba019e62b27790fd1c5c37ed1e29a1edc
darzwaitz <darzwaitz@yahoo.com>
2024-02-27 21:41:23 +0000

    styling Cart comp - TBU

--- pizza-shop/src/features/cart/Cart.jsx
+++ pizza-shop/src/features/cart/Cart.jsx
@@ -1,36 +1,37 @@
 // import { Link } from 'react-router-dom'
 import LinkButton from '../../ui/LinkButton'
 import Button from '../../ui/Button'
+import CartItem from '../cart/CartItem'
 
-// const fakeCart = [
-//   {
-//     pizzaId: 12,
-//     name: 'Mediterranean',
-//     quantity: 2,
-//     unitPrice: 16,
-//     totalPrice: 32,
-//   },
-//   {
-//     pizzaId: 6,
-//     name: 'Vegetale',
-//     quantity: 1,
-//     unitPrice: 13,
-//     totalPrice: 13,
-//   },
-//   {
-//     pizzaId: 11,
-//     name: 'Spinach and Mushroom',
-//     quantity: 1,
-//     unitPrice: 15,
-//     totalPrice: 15,
-//   },
-// ];
+const fakeCart = [
+    {
+        pizzaId: 12,
+        name: 'Mediterranean',
+        quantity: 2,
+        unitPrice: 16,
+        totalPrice: 32,
+    },
+    {
+        pizzaId: 6,
+        name: 'Vegetale',
+        quantity: 1,
+        unitPrice: 13,
+        totalPrice: 13,
+    },
+    {
+        pizzaId: 11,
+        name: 'Spinach and Mushroom',
+        quantity: 1,
+        unitPrice: 15,
+        totalPrice: 15,
+    },
+]
 
 function Cart() {
-    // const cart = fakeCart;
+    const cart = fakeCart
 
     return (
-        <div>
+        <div className="px-4 py-3 ">
             <LinkButton
                 to="/menu"
                 className="text-sm text-blue-500 hover:text-blue-600 hover:underline"
@@ -38,7 +39,13 @@ function Cart() {
                 &larr; Back to menu
             </LinkButton>
 
-            <h2>Your cart, %NAME%</h2>
+            <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+
+            <ul>
+                {cart.map((item) => (
+                    <CartItem item={item} key={item.key} />
+                ))}
+            </ul>
 
             <div>
                 <Button to="/order/new" type="primary">
