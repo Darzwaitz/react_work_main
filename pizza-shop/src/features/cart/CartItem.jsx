4183be9eb5dbd5a14542983470b91cb8ea04aa1d
darzwaitz <darzwaitz@yahoo.com>
2024-03-14 23:42:46 +0000

    updating cart quantity and calling a reducer on the slice with caseReducer method

--- pizza-shop/src/features/cart/CartItem.jsx
+++ pizza-shop/src/features/cart/CartItem.jsx
@@ -1,5 +1,6 @@
 import { formatCurrency } from '../../utils/helpers'
 import DeleteItem from './DeleteItem'
+import UpdateItemQuantity from './UpdateItemQuantity'
 
 function CartItem({ item }) {
     // const { pizzaId, name, quantity } = item
@@ -14,6 +15,8 @@ function CartItem({ item }) {
                 <p className="text-sm font-bold">
                     {formatCurrency(totalPrice)}
                 </p>
+
+                <UpdateItemQuantity pizzaId={pizzaId} />
                 <DeleteItem pizzaId={pizzaId} />
             </div>
         </li>
