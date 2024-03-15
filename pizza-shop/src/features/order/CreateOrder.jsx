49d0eb41ee625d652f6ea0ca7592d0945b100286
darzwaitz <darzwaitz@yahoo.com>
2024-03-15 23:24:44 +0000

    using cart for new orders - Part 1

--- pizza-shop/src/features/order/CreateOrder.jsx
+++ pizza-shop/src/features/order/CreateOrder.jsx
@@ -2,7 +2,9 @@
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom'
 import { createOrder } from '../../services/apiRestaurant'
 import Button from '../../ui/Button'
+import EmptyCart from '../cart/EmptyCart'
 import { useSelector } from 'react-redux'
+import { getCart } from '../cart/cartSlice'
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -10,29 +12,29 @@ const isValidPhone = (str) =>
         str
     )
 
-const fakeCart = [
-    {
-        pizzaId: 12,
-        name: 'Mediterranean',
-        quantity: 2,
-        unitPrice: 16,
-        totalPrice: 32,
-    },
-    {
-        pizzaId: 6,
-        name: 'Vegetale',
-        quantity: 1,
-        unitPrice: 13,
-        totalPrice: 13,
-    },
-    {
-        pizzaId: 11,
-        name: 'Spinach and Mushroom',
-        quantity: 1,
-        unitPrice: 15,
-        totalPrice: 15,
-    },
-]
+// const fakeCart = [
+//     {
+//         pizzaId: 12,
+//         name: 'Mediterranean',
+//         quantity: 2,
+//         unitPrice: 16,
+//         totalPrice: 32,
+//     },
+//     {
+//         pizzaId: 6,
+//         name: 'Vegetale',
+//         quantity: 1,
+//         unitPrice: 13,
+//         totalPrice: 13,
+//     },
+//     {
+//         pizzaId: 11,
+//         name: 'Spinach and Mushroom',
+//         quantity: 1,
+//         unitPrice: 15,
+//         totalPrice: 15,
+//     },
+// ]
 
 function CreateOrder() {
     const username = useSelector((state) => state.user.username)
@@ -42,8 +44,12 @@ function CreateOrder() {
 
     const formErrors = useActionData()
 
+    const cart = useSelector(getCart)
+
+    if (!cart.length) return <EmptyCart />
+
     // const [withPriority, setWithPriority] = useState(false);
-    const cart = fakeCart
+    // const cart = fakeCart
 
     return (
         <div className=" px-4 py-6">
