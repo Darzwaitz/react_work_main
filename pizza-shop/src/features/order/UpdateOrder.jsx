71f2f03891008fb23a8a95043ff5ca79dfd36df7
darzwaitz <darzwaitz@yahoo.com>
2024-04-01 23:58:10 +0100

    adding a button to set order to priority after adding items to cart - Part 4 - TBF

--- pizza-shop/src/features/order/UpdateOrder.jsx
+++ pizza-shop/src/features/order/UpdateOrder.jsx
@@ -1,14 +1,23 @@
 import { useFetcher } from 'react-router-dom'
 import Button from '../../ui/Button'
+import { updateOrder } from '../../services/apiRestaurant'
 
-function UpdateOrder({ order }) {
+// function UpdateOrder({ order }) {
+function UpdateOrder() {
     const fetcher = useFetcher()
 
     return (
-        <fetcher.Form method="PATCH" className=" text-right">
+        <fetcher.Form method="PATCH" className="text-right">
             <Button type="primary">Make Priority</Button>
         </fetcher.Form>
     )
 }
 
 export default UpdateOrder
+
+// eslint-disable-next-line no-unused-vars
+async function action({ params }) {
+    const data = { priority: true }
+    await updateOrder(params.orderId, data)
+    return null
+}
