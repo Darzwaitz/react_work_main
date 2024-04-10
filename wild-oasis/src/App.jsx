982efc779d3e0df728d3d5f0e00e951cac42ce56
darzwaitz <darzwaitz@yahoo.com>
2024-04-10 23:56:25 +0100

    building reusable layout comps in styled-components - Part 2

--- wild-oasis/src/App.jsx
+++ wild-oasis/src/App.jsx
@@ -17,20 +17,24 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Row type="horizontal">
-          <Heading as="h1">Header title</Heading>
-          <div>
-            <Heading as="h2">Check in - Check out</Heading>
-            <Button onClick={() => alert("tesht")}>Check In</Button>
-            <Button onClick={() => alert("tesht")}>Check Out</Button>
-          </div>
-        </Row>
-        <Row type="vertical">
-          <Heading as="h3">Form</Heading>
-          <form action="">
-            <Input type="number" placeholder="Number of guests" />
-            <Input type="number" placeholder="Number of guests" />
-          </form>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Header title</Heading>
+            <div>
+              <Heading as="h2">Check in - Check out</Heading>
+              <Button variation onClick={() => alert("tesht")}>
+                Check In
+              </Button>
+              <Button onClick={() => alert("tesht")}>Check Out</Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form action="">
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
         </Row>
       </StyledApp>
     </>
