263f2076654fa52da471e10012aeed28fb47ed72
darzwaitz <darzwaitz@yahoo.com>
2024-04-17 23:37:35 +0100

    setting up first query to supabase API

--- wild-oasis/src/services/apiCabins.js
+++ wild-oasis/src/services/apiCabins.js
@@ -1,1 +1,12 @@
-export async function getCabins() {}
+import supabase from "./supabase";
+
+export async function getCabins() {
+  let { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabinz couldnt be loaded");
+  }
+
+  return data;
+}
