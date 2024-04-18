62649bf9b66cf102208e0a40de85f58c16608f81
darzwaitz <darzwaitz@yahoo.com>
2024-04-18 23:19:50 +0100

    installed package tanstack react-query@4

--- wild-oasis/src/services/supabase.js
+++ wild-oasis/src/services/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://qgakobmnbmzjctbatgds.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
