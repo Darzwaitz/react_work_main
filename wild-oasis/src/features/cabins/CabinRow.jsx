43883c35937172474351ff47d675f568f755d732
darzwaitz <darzwaitz@yahoo.com>
2024-05-10 21:45:53 +0100

    backup CreateCabinForm comp. & duplicated it for refactor for edit existing cabin function

--- wild-oasis/src/features/cabins/CabinRow.jsx
+++ wild-oasis/src/features/cabins/CabinRow.jsx
@@ -90,7 +90,7 @@ function CabinRow({ cabin }) {
           </button>
         </div>
       </TableRow>
-      {showForm && <CreateCabinForm />}
+      {showForm && <CreateCabinForm cabinToEdit={cabin} />}
     </>
   );
 }
