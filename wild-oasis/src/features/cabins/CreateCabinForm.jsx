9500d3c44a2a3e5ed5cd3f5f5352a5ce74f9232f
darzwaitz <darzwaitz@yahoo.com>
2024-05-15 23:46:35 +0100

    reusing the mutate function for creating a cabin to editing a cabin - Part 1

--- wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -24,10 +24,22 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
   const queryClient = useQueryClient();
 
-  const { mutate, isLoading: isCreating } = useMutation({
+  const { mutate: createCabin, isLoading: isCreating } = useMutation({
     mutationFn: createEditCabin,
     onSuccess: () => {
       toast.success("New cabin successfully created");
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+      reset();
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  const { mutate: editCabin, isLoading: isEditing } = useMutation({
+    mutationFn: createEditCabin,
+    onSuccess: () => {
+      toast.success("Cabin successfully edited");
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
