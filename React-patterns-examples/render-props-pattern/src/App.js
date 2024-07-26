13fe093ec9568e73eb0a3a4b8a35dddcb0eb5e47
darzwaitz <darzwaitz@yahoo.com>
2024-07-26 23:41:40 +0100

    building a higher order component - Part 1

--- React-patterns-examples/render-props-pattern/src/App.js
+++ React-patterns-examples/render-props-pattern/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { faker } from "@faker-js/faker";
 import "./styles.css";
+import withToggles from "./HOC";
 
 const products = Array.from({ length: 20 }, () => {
   return {
@@ -74,6 +75,19 @@ function List({ title, items, render }) {
   );
 }
 
+// LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
+function ProductList({ title, items }) {
+  return (
+    <ul className="list">
+      {items.map((product) => (
+        <ProductItem key={product.productName} product={product} />
+      ))}
+    </ul>
+  );
+}
+
+const ProductListWithToggles = withToggles(ProductList);
+
 export default function App() {
   return (
     <div>
@@ -100,16 +114,5 @@ export default function App() {
         />
       </div>
     </div>
-  );
-}
-
-// LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
-function ProductList({ title, items }) {
-  return (
-    <ul className="list">
-      {items.map((product) => (
-        <ProductItem key={product.productName} product={product} />
-      ))}
-    </ul>
   );
 }
