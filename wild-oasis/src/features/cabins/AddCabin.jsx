98ef7e6b14f3caf76fb69782f22e1db3710495ac
darzwaitz <darzwaitz@yahoo.com>
2024-08-07 23:41:27 +0100

    modal comp fully refactored to reusable compound comp - 2nd instance of created

--- wild-oasis/src/features/cabins/AddCabin.jsx
+++ wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,6 +1,7 @@
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
+import CabinTable from "./CabinTable";
 
 function AddCabin() {
   return (
@@ -10,6 +11,13 @@ function AddCabin() {
       </Modal.Open>
       <Modal.Window name="cabin-form">
         <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button>Show Table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
       </Modal.Window>
     </Modal>
   );
