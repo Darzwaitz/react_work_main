1c97d3f79b5a5074fc78554cd39ab06988392842
darzwaitz <darzwaitz@yahoo.com>
2024-10-03 23:40:59 +0100

    setting up API side pagination - Part 2 - and created a constants file for a global constant

--- wild-oasis/src/ui/Pagination.jsx
+++ wild-oasis/src/ui/Pagination.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { VscChevronLeft } from "react-icons/vsc";
 import { VscChevronRight } from "react-icons/vsc";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../utils/constants";
 
 const StyledPagination = styled.div`
   width: 100%;
@@ -58,8 +59,6 @@ const PaginationButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
-
-const PAGE_SIZE = 10;
 
 function Pagination({ count }) {
   const [searchParams, setSearchParams] = useSearchParams();
