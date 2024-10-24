e5f639d86ac452c55e40613a7ec99aecb0488efa
darzwaitz <darzwaitz@yahoo.com>
2024-10-24 23:56:15 +0100

    deleting a booking - Part 1 - TBF

--- wild-oasis/src/features/bookings/BookingRow.jsx
+++ wild-oasis/src/features/bookings/BookingRow.jsx
@@ -4,11 +4,14 @@ import { format, isToday } from "date-fns";
 import Tag from "../../ui/Tag";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
 
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
 import { useNavigate } from "react-router-dom";
-import useCheckout from "../check-in-out/useCheckout";
+import { useCheckout } from "../check-in-out/useCheckout";
+import { useDeleteBooking } from "./useDeleteBooking";
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -53,6 +56,8 @@ function BookingRow({
 }) {
   const navigate = useNavigate();
   const { checkout, isCheckingOut } = useCheckout();
+  // const { isDeleting, deleteBooking } = useDeleteBooking();
+  const { deleteBooking } = useDeleteBooking();
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -88,39 +93,52 @@ function BookingRow({
 
       <Amount>{formatCurrency(totalPrice)}</Amount>
 
-      <Menus.Menu>
-        <Menus.Toggle id={bookingId} />
-        <Menus.List id={bookingId}>
-          {/* eye icon to insert */}
-          <Menus.Button
-            icon={"*icon"}
-            onClick={() => navigate(`/bookings/${bookingId}`)}
-          >
-            See details
-          </Menus.Button>
-
-          {/* arrow down icon to insert */}
-          {status === "unconfirmed" && (
+      <Modal>
+        <Menus.Menu>
+          <Menus.Toggle id={bookingId} />
+          <Menus.List id={bookingId}>
+            {/* eye icon to insert */}
             <Menus.Button
               icon={"*icon"}
-              onClick={() => navigate(`/checkin/${bookingId}`)}
+              onClick={() => navigate(`/bookings/${bookingId}`)}
             >
-              Check in
+              See details
             </Menus.Button>
-          )}
 
-          {/* arrow down icon to insert */}
-          {status === "checked-in" && (
-            <Menus.Button
-              icon={"*icon"}
-              onClick={() => checkout(bookingId)}
-              disabled={isCheckingOut}
-            >
-              Check out
-            </Menus.Button>
-          )}
-        </Menus.List>
-      </Menus.Menu>
+            {/* arrow down icon to insert */}
+            {status === "unconfirmed" && (
+              <Menus.Button
+                icon={"*icon"}
+                onClick={() => navigate(`/checkin/${bookingId}`)}
+              >
+                Check in
+              </Menus.Button>
+            )}
+
+            {/* arrow down icon to insert */}
+            {status === "checked-in" && (
+              <Menus.Button
+                icon={"*icon"}
+                onClick={() => checkout(bookingId)}
+                disabled={isCheckingOut}
+              >
+                Check out
+              </Menus.Button>
+            )}
+
+            <Modal.Open opens="delete">
+              <Menus.Button icon={"icon"}>Delete booking</Menus.Button>
+            </Modal.Open>
+          </Menus.List>
+        </Menus.Menu>
+
+        <Modal.Window name="delete">
+          <ConfirmDelete
+            resourceName="booking"
+            onConfirm={() => deleteBooking(bookingId)}
+          />
+        </Modal.Window>
+      </Modal>
     </Table.Row>
   );
 }
