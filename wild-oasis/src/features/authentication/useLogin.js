c908ebcde7ea9bbca4c79066004be36c31b47b57
darzwaitz <darzwaitz@yahoo.com>
2024-11-01 23:58:20 +0000

    setting up authentication with supabase - user login - Part 5 - updated LoginForm

--- wild-oasis/src/features/authentication/useLogin.js
+++ wild-oasis/src/features/authentication/useLogin.js
@@ -1,12 +1,26 @@
 import { useMutation } from "@tanstack/react-query";
-import { login } from "../../services/apiAuth";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 export function useLogin() {
-  useMutation({
+  const navigate = useNavigate();
+
+  const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) =>
-      login({
+      loginApi({
         email,
         password,
       }),
+    onSuccess: (user) => {
+      console.log(user);
+      navigate("/dashboard");
+    },
+    onError: (err) => {
+      console.log("ERROR", err);
+      toast.error("Provided email or password are incorrect");
+    },
   });
+
+  return { login, isLoading };
 }
