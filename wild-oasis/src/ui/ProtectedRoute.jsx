5a15df35a8b864043a416fa5b8f91c9bd9fc8ad2
darzwaitz <darzwaitz@yahoo.com>
2024-11-04 23:58:58 +0000

    setting up authorization with a protected parent comp/route - Part 2 - created custom hook

--- wild-oasis/src/ui/ProtectedRoute.jsx
+++ wild-oasis/src/ui/ProtectedRoute.jsx
@@ -1,7 +1,14 @@
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+
 function ProtectedRoute({ children }) {
   // 1. Load authenticated user
+  //   const { user, isLoading } = useUser();
+  const { isLoading } = useUser();
 
   // 2. while loading, show spinner
+  if (isLoading) return <Spinner />;
+
   // 3. If NO authenticated user, redirect to /login
   // 4. If there IS user, render the app
 
