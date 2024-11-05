ae92b4785fc7b3b373850cd6054a1ec2d78f4b42
darzwaitz <darzwaitz@yahoo.com>
2024-11-05 23:58:22 +0000

    setting up authorization - Part 4 - checking if user is authenticated in current session

--- wild-oasis/src/services/apiAuth.js
+++ wild-oasis/src/services/apiAuth.js
@@ -16,8 +16,9 @@ export async function getCurrentUser() {
   const { data: session } = await supabase.auth.getSession();
   if (!session.session) return null;
 
+  // safer to re-download data
   const { data, error } = await supabase.auth.getUser();
-  console.log(data);
+  // console.log(data);
 
   if (error) throw new Error(error.message);
 
--- wild-oasis/src/ui/ProtectedRoute.jsx
+++ wild-oasis/src/ui/ProtectedRoute.jsx
@@ -1,5 +1,14 @@
+import styled from "styled-components";
 import { useUser } from "../features/authentication/useUser";
 import Spinner from "./Spinner";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 function ProtectedRoute({ children }) {
   // 1. Load authenticated user
@@ -7,7 +16,10 @@ function ProtectedRoute({ children }) {
   const { isLoading } = useUser();
 
   // 2. while loading, show spinner
-  if (isLoading) return <Spinner />;
+  if (isLoading) return;
+  <FullPage>
+    <Spinner />
+  </FullPage>;
 
   // 3. If NO authenticated user, redirect to /login
   // 4. If there IS user, render the app
