7d6d34e3304eac0936317487c75df12b8eec9da5
darzwaitz <darzwaitz@yahoo.com>
2024-11-06 23:58:48 +0000

    setting up authorization - Part 5 - show login if user is not authenticated

--- wild-oasis/src/ui/ProtectedRoute.jsx
+++ wild-oasis/src/ui/ProtectedRoute.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { useUser } from "../features/authentication/useUser";
 import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const FullPage = styled.div`
   height: 100vh;
@@ -11,18 +13,28 @@ const FullPage = styled.div`
 `;
 
 function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
   // 1. Load authenticated user
   //   const { user, isLoading } = useUser();
-  const { isLoading } = useUser();
+  const { isLoading, isAuthenticated } = useUser();
 
-  // 2. while loading, show spinner
+  // 2. If NO authenticated user, redirect to /login
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  // 3. while loading, show spinner
   if (isLoading) return;
   <FullPage>
     <Spinner />
   </FullPage>;
 
-  // 3. If NO authenticated user, redirect to /login
   // 4. If there IS user, render the app
+  if (isAuthenticated) return children;
 
   return children;
 }
