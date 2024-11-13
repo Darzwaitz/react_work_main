37ecb2da44817db6435a2987a862c686afc70583
darzwaitz <darzwaitz@yahoo.com>
2024-11-13 23:49:05 +0000

    setting up create new user form using useForm from react-hook-form - form validation working

--- wild-oasis/src/pages/Users.jsx
+++ wild-oasis/src/pages/Users.jsx
@@ -1,70 +1,11 @@
+import SignupForm from "../features/authentication/SignupForm";
 import Heading from "../ui/Heading";
 
 function NewUsers() {
   return (
     <>
       <Heading as="h1">Create a new user</Heading>
-      <section>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-        velit, tempore dicta, molestias a totam ipsam, similique animi modi
-        laborum quibusdam facilis eos! Numquam rerum alias fuga magni dolor esse
-        perferendis quas nam repudiandae. Optio animi odio maiores expedita
-        repellat ullam, ipsum quia, voluptatum sit omnis aliquid enim
-        recusandae, minima officia atque dicta vero dolorem corrupti id!
-        Molestias eveniet distinctio molestiae error odit amet nam nihil officia
-        veniam facilis illo quaerat voluptatem repudiandae repellendus voluptas
-        excepturi rem maxime corporis, ipsum similique quidem? Modi ipsam dicta,
-        sint vel distinctio magni odio numquam est voluptates repellat magnam
-        corporis architecto aliquam recusandae vitae iure inventore fugiat
-        quibusdam perspiciatis esse consectetur sed doloremque. In aliquid
-        tenetur quas dolorem deleniti consequatur libero, eum eius possimus ea
-        neque commodi repudiandae voluptas alias, earum doloribus autem quidem
-        qui aliquam, tempore voluptate consequuntur? Labore sed in natus
-        nesciunt temporibus at similique, veritatis tempora esse earum placeat
-        magnam ea doloremque cum dolores recusandae dolor laborum. Facilis
-        quibusdam voluptas tempora maiores, quisquam quasi officiis voluptate et
-        corporis minima? Quia, reiciendis praesentium reprehenderit at voluptate
-        earum enim ullam, quidem assumenda molestiae sit porro dignissimos eum
-        dolores quibusdam eligendi veniam blanditiis asperiores. Deleniti animi
-        ipsa, corporis blanditiis reprehenderit maiores ullam nostrum provident
-        earum iste totam cum labore veniam ipsam esse alias. Perferendis,
-        placeat vel molestias, repellendus delectus libero dicta quaerat quae
-        voluptate optio minus dolores, exercitationem mollitia eveniet alias
-        quis aut eius. Repudiandae fugiat debitis, enim eveniet illum eaque
-        impedit laborum nulla et quos modi qui alias explicabo, autem eligendi
-        in commodi molestiae iste ut maxime amet? Hic nesciunt ut officiis iusto
-        nemo quis cumque, perferendis vel tempore itaque neque illum doloremque,
-        quia vero fuga sit velit eos deleniti corporis temporibus quam. Vel
-        ipsum odio sequi inventore natus perferendis, laboriosam sint minima,
-        officiis doloribus assumenda perspiciatis recusandae nihil nisi quam,
-        voluptates unde quasi ipsam. Magni quia ea ab odio molestiae eius nam.
-        Quo sed, blanditiis sunt fuga esse aut, asperiores expedita autem saepe
-        vero repudiandae, quod accusantium voluptate odio delectus aperiam?
-        Laborum minus sequi asperiores? Ducimus totam harum a, quia nulla in
-        fuga, mollitia ipsa vitae quisquam libero cum? Officiis, odio nobis.
-        Unde officia inventore labore assumenda repudiandae, eos aut amet
-        reprehenderit quis itaque accusamus architecto praesentium voluptatibus
-        hic officiis quos dignissimos sed. Adipisci quos, qui itaque commodi
-        distinctio quibusdam error veniam nesciunt, quisquam hic ipsum modi.
-        Vitae rerum laborum pariatur quisquam iste ea labore perferendis
-        officiis soluta natus praesentium sunt vel blanditiis ducimus, nemo
-        dolores exercitationem eligendi accusantium corporis sapiente! Id
-        eligendi quos, odio labore esse odit vitae, neque est quidem illo quas
-        atque, quod quaerat cupiditate voluptate nobis porro libero ipsa
-        veritatis? Distinctio vel similique in ex velit reprehenderit voluptates
-        doloribus labore accusamus voluptatem, omnis, delectus maxime deleniti,
-        ducimus ea laudantium ut sint repellat! Inventore error soluta
-        asperiores dolor dolores, a fugiat ad eveniet, molestiae quisquam enim
-        aliquam? Quo cupiditate dolorem magnam veritatis similique debitis nisi
-        iusto tempora velit eveniet, officiis possimus numquam saepe magni
-        facilis laborum iure accusamus autem sint quisquam voluptatum suscipit
-        sequi repellendus ab? Accusamus unde hic eligendi debitis? Quibusdam cum
-        illum cumque? Architecto cupiditate eaque ullam odio tempore placeat
-        iste non quas, ratione quam quaerat nisi inventore ex autem consectetur
-        deserunt qui quia animi quasi assumenda! Expedita assumenda eius alias
-        repellendus placeat odit voluptatum ut inventore pariatur neque, tenetur
-        temporibus corrupti perspiciatis quis facere sapiente dolorum!
-      </section>
+      <SignupForm />
     </>
   );
 }
